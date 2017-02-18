72429d418d52b378fc40b1df5d70ef8d049545d6
Atomist <cli@atomist.com>
2017-02-18 15:24:02 +0000

    just put positionedStructure() around my parsers
    ```
    ---
    kind: "operation"
    client: "rug-cli 0.23.1-SNAPSHOT"
    editor:
      name: "WrapFunctionBody"
      group: "jessitron"
      artifact: "kats-rugs"
      version: "0.1.0"
      origin:
        repo: "git@github.com:jessitron/kats-rugs.git"
        branch: "master"
        sha: "26ac88b*"
    
    ```

--- src/main/scala/com/jessitron/kats/ElmParser.scala
+++ src/main/scala/com/jessitron/kats/ElmParser.scala
@@ -6,50 +6,50 @@ object ElmParser extends RegexParsers {
 
   def uppercaseIdentifier(name: String) : Parser[PositionedSyntaxNode] = positionedNode("[A-Z][A-Za-z0-9_]*".r ^^ SyntaxNode.leaf(name))
 
-  def lowercaseIdentifier(name: String) : Parser[SyntaxNode] = "[a-z][A-Za-z0-9_]*".r ^^ SyntaxNode.leaf(name)
+  def lowercaseIdentifier(name: String) : Parser[PositionedSyntaxNode] = positionedNode("[a-z][A-Za-z0-9_]*".r ^^ SyntaxNode.leaf(name))
 
   def exposure = lowercaseIdentifier("exposedFunction") | uppercaseIdentifier("exposedType")
 
   def exposings: Parser[Seq[SyntaxNode]] = "exposing" ~ "(" ~> rep1sep(exposure, ",") <~ ")"
 
 
-  def moduleDeclaration: Parser[SyntaxNode] = "module" ~> uppercaseIdentifier("moduleName") ~ exposings ^^
-  { case name ~ exposings => SyntaxNode.parent("moduleDeclaration", Seq(name) ++  exposings)}
+  def moduleDeclaration: Parser[PositionedSyntaxNode] = positionedNode("module" ~> uppercaseIdentifier("moduleName") ~ exposings ^^
+  { case name ~ exposings => SyntaxNode.parent("moduleDeclaration", Seq(name) ++  exposings)})
 
-  def importStatement: Parser[SyntaxNode] = "import" ~> qualifiedUppercaseIdentifier("importName") ~ opt(exposings) ^^
-    { case name ~ exposings => SyntaxNode.parent("importStatement", Seq(name) ++ exposings.toSeq.flatten)}
+  def importStatement: Parser[PositionedSyntaxNode] = positionedNode("import" ~> qualifiedUppercaseIdentifier("importName") ~ opt(exposings) ^^
+    { case name ~ exposings => SyntaxNode.parent("importStatement", Seq(name) ++ exposings.toSeq.flatten)})
 
 
   import ElmFunction.functionDeclaration
-  def elmModule: Parser[SyntaxNode] = moduleDeclaration ~ rep(importStatement) ~ rep(functionDeclaration) ^^ {
+  def elmModule: Parser[PositionedSyntaxNode] = positionedNode(moduleDeclaration ~ rep(importStatement) ~ rep(functionDeclaration) ^^ {
     case name ~ imports ~ fns => SyntaxNode.parent("elmModule", Seq(name) ++ imports ++ fns)
-  }
+  })
 
 
-  def qualifiedLowercaseIdentifier(name: String): Parser[SyntaxNode] = opt(rep1sep(uppercaseIdentifier("component"), ".") <~ ".") ~ lowercaseIdentifier(name) ^^ {
+  def qualifiedLowercaseIdentifier(name: String): Parser[PositionedSyntaxNode] = positionedNode(opt(rep1sep(uppercaseIdentifier("component"), ".") <~ ".") ~ lowercaseIdentifier(name) ^^ {
     case None ~ p => SyntaxNode.parent(name, Seq(p))
     case Some(packages) ~ p => SyntaxNode.parent(name, packages :+ p)
-  }
+  })
 
-  def qualifiedUppercaseIdentifier(name: String): Parser[SyntaxNode] = rep1sep(uppercaseIdentifier("component"), ".") ^^ {
+  def qualifiedUppercaseIdentifier(name: String): Parser[PositionedSyntaxNode] = positionedNode(rep1sep(uppercaseIdentifier("component"), ".") ^^ {
     SyntaxNode.parent(name, _)
-  }
+  })
 
   object ElmExpression {
 
-    def expression(name: String): Parser[SyntaxNode] = (functionApplication | listLiteral | stringLiteral) ^^ {
+    def expression(name: String): Parser[PositionedSyntaxNode] = positionedNode((functionApplication | listLiteral | stringLiteral) ^^ {
       case expr => SyntaxNode.parent(name, Seq(expr))
-    }
+    })
 
-    def functionApplication: Parser[SyntaxNode] = qualifiedLowercaseIdentifier("calledFunction") ~ rep(expression("argument")) ^^ {
+    def functionApplication: Parser[PositionedSyntaxNode] = positionedNode(qualifiedLowercaseIdentifier("calledFunction") ~ rep(expression("argument")) ^^ {
       case fn ~ args => SyntaxNode.parent("functionApplication", fn +: args)
-    }
+    })
 
-    def listLiteral: Parser[SyntaxNode] = "[" ~> repsep(expression("listItem"), ",") <~ "]" ^^ {
+    def listLiteral: Parser[PositionedSyntaxNode] = positionedNode("[" ~> repsep(expression("listItem"), ",") <~ "]" ^^ {
       items => SyntaxNode.parent("listLiteral", items)
-    }
+    })
 
-    def stringLiteral: Parser[SyntaxNode] = """"[^"]*"""".r ^^ SyntaxNode.leaf("stringLiteral")
+    def stringLiteral: Parser[PositionedSyntaxNode] = positionedNode(""""[^"]*"""".r ^^ SyntaxNode.leaf("stringLiteral"))
 
   }
 
@@ -57,18 +57,18 @@ object ElmParser extends RegexParsers {
 
   object ElmFunction {
 
-    def functionDeclaration: Parser[SyntaxNode] =
-      opt(functionTypeDeclaration) ~ lowercaseIdentifier("functionName") ~ "=" ~ expression("body") ^^ {
+    def functionDeclaration: Parser[PositionedSyntaxNode] =
+      positionedNode(opt(functionTypeDeclaration) ~ lowercaseIdentifier("functionName") ~ "=" ~ expression("body") ^^ {
         case typeOption ~ name ~ "=" ~ body => SyntaxNode.parent("functionDeclaration", typeOption.toSeq ++ Seq(name, body))
-      }
+      })
 
-    private  def functionTypeDeclaration: Parser[SyntaxNode] = lowercaseIdentifier("functionName") ~ ":" ~ elmType("declaredType") ^^ {
+    private    def functionTypeDeclaration: Parser[PositionedSyntaxNode] = positionedNode(lowercaseIdentifier("functionName") ~ ":" ~ elmType("declaredType") ^^ {
       case name ~ ":" ~ typ => SyntaxNode.parent("typeDeclaration", Seq(name, typ))
-    }
+    })
 
-    private  def elmType(name: String): Parser[SyntaxNode] = uppercaseIdentifier("typeName") ~ rep(elmType("typeParameter")) ^^ {
+    private    def elmType(name: String): Parser[PositionedSyntaxNode] = positionedNode(uppercaseIdentifier("typeName") ~ rep(elmType("typeParameter")) ^^ {
       case typeName ~ parameters => SyntaxNode.parent(name, typeName +: parameters)
-    }
+    })
 
   }
 
@@ -101,6 +101,7 @@ object ElmParser extends RegexParsers {
 case class SyntaxNode(name: String,
                       childNodes: Seq[PositionedSyntaxNode],
                       valueOption: Option[String])
+
 
 object SyntaxNode {
 
