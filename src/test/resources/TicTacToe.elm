0de10b88687791d637001c38a9b50e6a16e63054
Jessica Kerr <jessitron@gmail.com>
2017-07-10 17:06:58 -0500

    this does not finish

--- src/test/resources/TicTacToe.elm
+++ src/test/resources/TicTacToe.elm
@@ -31,29 +31,3 @@ draw : Maybe Move -> Html msg
 draw move =
     move |> toString |> Html.text
 
-
-borderStyle =
-    style [ ( "border", "1px solid red" ) ]
-
-
-main : Html Never
-main =
-    case emptyBoard of
-        TicTacToe a1 a2 a3 b1 b2 b3 c1 c2 c3 ->
-            Html.table []
-                [ Html.tr []
-                    [ Html.td [ borderStyle ] [ draw a1 ]
-                    , Html.td [ borderStyle ] [ draw a2 ]
-                    , Html.td [ borderStyle ] [ draw a3 ]
-                    ]
-                , Html.tr []
-                    [ Html.td [ borderStyle ] [ draw b1 ]
-                    , Html.td [ borderStyle ] [ draw b2 ]
-                    , Html.td [ borderStyle ] [ draw b3 ]
-                    ]
-                , Html.tr []
-                    [ Html.td [ borderStyle ] [ draw c1 ]
-                    , Html.td [ borderStyle ] [ draw c2 ]
-                    , Html.td [ borderStyle ] [ draw c3 ]
-                    ]
-                ]
