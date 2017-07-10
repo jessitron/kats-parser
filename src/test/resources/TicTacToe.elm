a0d6b95b1fe55d004c35acc27d590e91bb96969e
Jessica Kerr <jessitron@gmail.com>
2017-07-10 17:16:35 -0500

    This doesn't appear to complete but I think it's just slow
    Like, when I delete them and add them back one at a time, it gets slower and slower

--- src/test/resources/TicTacToe.elm
+++ src/test/resources/TicTacToe.elm
@@ -1,25 +1,16 @@
 module Main exposing (main)
 
-import Html exposing (Html)
-import Html.Attributes exposing (style)
-
-
-type Move
-    = X
-    | O
-
-
-type TicTacToe
-    = TicTacToe (Maybe Move) (Maybe Move) (Maybe Move) (Maybe Move) (Maybe Move) (Maybe Move) (Maybe Move) (Maybe Move) (Maybe Move)
-
-
 emptyBoard : TicTacToe
 emptyBoard =
     TicTacToe
         Nothing
         Nothing
+        Nothing
+        Nothing
+        Nothing
+        Nothing
+        Nothing
+        Nothing
+        Nothing
 
-draw : Maybe Move -> Html msg
-draw move =
-    move |> toString |> Html.text
 
